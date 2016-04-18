c324ca49e914690d5a2564ca6979a57bce0c043c
Julian Cheal <jcheal@redhat.com>
2016-04-18 01:51:07 +0100

    Major version 1.0.0
    Well as we've broken backwards compatability may as well 1.0 this thing

--- lib/ofx/version.rb
+++ lib/ofx/version.rb
@@ -1,8 +1,8 @@
 module OFX
   module Version
-    MAJOR = 0
-    MINOR = 4
-    PATCH = 2
-    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
+    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}".freeze
   end
 end
